00bb511730337d6ab002358ce48045ed7e0f5bbb
Artem <home@MacBook-Air-Artem.local>
2025-09-06 15:42:43 +0300

    test_auth_endpoint

--- api/login_handlers.py
+++ api/login_handlers.py
@@ -5,10 +5,13 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from jose import JWTError
+from schemas import Token
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.schemas import Token
 from db.models import User
 from db.repositories import UserRepository
 from db.session import get_db
@@ -42,18 +45,51 @@ async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: AsyncSession = Depends(get_db),
 ):
-    user = await authenticate_user(form_data.username, form_data.password, db)
+    user = await authenticate_user(form_data.email, form_data.password, db)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
 
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.email, "other_custom_data": [1, 2, 3, 41]},
+        data={"sub": user.email, "other_custom_data": [1, 2, 3, 4]},
         expires_delta=access_token_expires,
     )
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+
+
+async def get_current_user_from_token(
+    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        email: str = payload.get("sub")
+        print("email extracted is ", email)
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await _fetch_user_by_email(email=email, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@login_router.get("/test_auth_endpoint")
+async def sample_endpoint_under_jw(
+    current_user: User = Depends(get_current_user_from_token),
+):
+    return {"Success": True, "current_user": current_user}
