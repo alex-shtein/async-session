1db7357609a33f4c4436ea639bbeb2b7e8d4f435
Artem <home@MacBook-Air-Artem.local>
2025-09-06 16:03:55 +0300

    refactoring

--- api/login_handlers.py
+++ api/login_handlers.py
@@ -22,17 +22,17 @@ from utils.security import create_access_token
 login_router = APIRouter()
 
 
-async def _fetch_user_by_email(email: str, db: AsyncSession) -> Optional[User]:
-    async with db as session:
+async def get_user_by_email(email: str, db_session: AsyncSession) -> Optional[User]:
+    async with db_session as session:
         async with session.begin():
-            repo = UserRepository(session)
-            return await repo.get_by_email(email=email)
+            user_repository = UserRepository(session)
+            return await user_repository.get_by_email(email=email)
 
 
 async def authenticate_user(
-    email: str, password: str, db: AsyncSession
+    email: str, password: str, db_session: AsyncSession
 ) -> Optional[User]:
-    user = await _fetch_user_by_email(email=email, db=db)
+    user = await get_user_by_email(email=email, db_session=db_session)
     if user is None:
         return None
     if not Hasher.verify_password(password, user.hashed_password):
@@ -43,9 +43,9 @@ async def authenticate_user(
 @login_router.post("/token", response_model=Token)
 async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
-    db: AsyncSession = Depends(get_db),
+    db_session: AsyncSession = Depends(get_db),
 ):
-    user = await authenticate_user(form_data.email, form_data.password, db)
+    user = await authenticate_user(form_data.username, form_data.password, db_session)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -53,10 +53,10 @@ async def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expiration = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.email, "other_custom_data": [1, 2, 3, 4]},
-        expires_delta=access_token_expires,
+        expires_delta=access_token_expiration,
     )
 
     return {"access_token": access_token, "token_type": "bearer"}
@@ -66,7 +66,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
 
 
 async def get_current_user_from_token(
-    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
+    token: str = Depends(oauth2_scheme), db_session: AsyncSession = Depends(get_db)
 ):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -77,19 +77,19 @@ async def get_current_user_from_token(
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
         )
         email: str = payload.get("sub")
-        print("email extracted is ", email)
+        print("Extracted email: ", email)
         if email is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-    user = await _fetch_user_by_email(email=email, db=db)
+    user = await get_user_by_email(email=email, db_session=db_session)
     if user is None:
         raise credentials_exception
     return user
 
 
 @login_router.get("/test_auth_endpoint")
-async def sample_endpoint_under_jw(
+async def sample_endpoint_under_jwt(
     current_user: User = Depends(get_current_user_from_token),
 ):
     return {"Success": True, "current_user": current_user}
